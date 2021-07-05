6aeb54831e698620aab2c81bec13f6a6244deb3a
Mohammad Hossein Vafaee <vafaee.mh@gmail.com>
2021-07-05 16:35:00 +0430

    finish the menu Hamberger

--- src/app/Components/NavigationMobile/NavigationMobile.js
+++ src/app/Components/NavigationMobile/NavigationMobile.js
@@ -1,16 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import classes from './NavigationMobile.module.scss';
 import NavLink from "../NavLink/NavLink";
 import {CgMenuRound} from "react-icons/all";
 
 
 export default function NavigationMobile() {
-
+const [open ,setOpen]=useState(false)
     return (
 
         <nav className={classes.NavigationMobile}>
-            <CgMenuRound className={classes.Hamburger}  />
-            <NavLink/>
+            <CgMenuRound onClick={()=>{setOpen(!open)}} className={classes.Hamburger}  />
+            {open &&
+                <NavLink/>}
         </nav>
 
     )
