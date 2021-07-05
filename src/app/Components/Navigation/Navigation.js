397af283c5cdbf6f3e5d5a3ddcd777c7146bf3e8
Mohammad Hossein Vafaee <vafaee.mh@gmail.com>
2021-07-05 11:59:13 +0430

    build hamburgerMenu

--- src/app/Components/Navigation/Navigation.js
+++ src/app/Components/Navigation/Navigation.js
@@ -1,28 +1,30 @@
-import React from "react";
+import React, {useRef} from "react";
 import {Link} from "react-router-dom";
 import classes from './Navigation.module.scss';
 import {Constants} from "../../values/Constants";
 
 
-
-
 export default function Navigation() {
+    const Items = useRef()
     return (
 
-            <nav className={classes.Navigation}>
-                <ul className={classes.Items}>
-                    <li className={classes.Item}><Link to="/" className={classes.Link}>{Constants.Splash}</Link></li>
-                    <li className={classes.Item}>
-                        <Link to={'/product'} className={classes.Link}>
-                           {Constants.Product}
-                        </Link>
-                    </li>
-                    <li className={classes.Item}><Link to="/product/laptop" className={classes.Link}>{Constants.Laptop}</Link></li>
-                    <li className={classes.Item}><Link to="/product/camera" className={classes.Link}>{Constants.Camera}</Link></li>
-                    <li className={classes.Item}><Link to="/product/watch" className={classes.Link}>{Constants.Watch}</Link></li>
-                    <li className={classes.Item}><Link to='/cartBox' className={classes.Link}>{Constants.Cart}</Link></li>
-                </ul>
-            </nav>
+        <nav className={classes.Navigation}>
+            <ul ref={Items} className={classes.Items}>
+                <li className={classes.Item}><Link to="/" className={classes.Link}>{Constants.Splash}</Link></li>
+                <li className={classes.Item}>
+                    <Link to={'/product'} className={classes.Link}>
+                        {Constants.Product}
+                    </Link>
+                </li>
+                <li className={classes.Item}><Link to="/product/laptop"
+                                                   className={classes.Link}>{Constants.Laptop}</Link></li>
+                <li className={classes.Item}><Link to="/product/camera"
+                                                   className={classes.Link}>{Constants.Camera}</Link></li>
+                <li className={classes.Item}><Link to="/product/watch" className={classes.Link}>{Constants.Watch}</Link>
+                </li>
+                <li className={classes.Item}><Link to='/cartBox' className={classes.Link}>{Constants.Cart}</Link></li>
+            </ul>
+        </nav>
 
     )
 }
--- src/app/Layout/Basic.layout.js
+++ src/app/Layout/Basic.layout.js
@@ -1,17 +1,32 @@
-import React from "react";
+import React, {useRef, useState} from "react";
 import {Link} from "react-router-dom";
 import classes from './Basic.layout.module.scss'
 import Navigation from "../Components/Navigation/Navigation";
 import {Logo} from "../values/Files";
 import Footer from "../Components/Footer/Footer";
 import CartBoxHeader from "../Components/CartBoxHeader/CartBoxHeader";
+import {GiHamburgerMenu} from "react-icons/all";
 
 
 export default function BasicLayout(props) {
+    const [isClose, setIsClose] = useState(true);
+    const nav=props.ref
+    const menuHamburger = () => {
+        if (isClose) {
+            nav.classList.add('active');
+            setIsClose(false)
+        }else {
+            nav.classList.remove('active')
+            setIsClose(true)
+        }
+    }
     return (
         <div className={classes.BasicLayout}>
 
             <header className={classes.Header}>
+                <span onClick={menuHamburger} className={classes.HamburgerMenu}>
+                    <GiHamburgerMenu/>
+                </span>
                 <Navigation/>
                 <CartBoxHeader/>
                 <div className={classes.BoxLogo}>
