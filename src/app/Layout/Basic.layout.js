0b43cbb77900c95613ffc248c9245969466a3f7b
Mohammad Hossein Vafaee <vafaee.mh@gmail.com>
2021-07-08 13:28:39 +0430

    work on responsiv the splash page

--- src/app/Layout/Basic.layout.js
+++ src/app/Layout/Basic.layout.js
@@ -12,43 +12,44 @@ export default function BasicLayout(props) {
 
     return (
         <div className={classes.BasicLayout}>
-            <div className={'container'}>
-                <div className={'row'}>
-                    <header className={classes.Header}>
-                        <div className="col-md-8 col-xs-2">
-                            <Navigation/>
-                            <NavigationMobile/>
-                        </div>
-                        <div className="col-md-4 col-xs-10">
-                            <div className={'row'}>
-                                <div className={classes.Left}>
-                                    <div className="col-md-6 col-xs-6">
-                                        <div className={classes.BoxCart}>
-                                            <CartBoxHeader/>
-                                        </div>
-                                    </div>
-                                    <div className="col-md-6 col-xs-6">
-                                        <div className={classes.BoxLogo}>
-                                            <Link to="/">
-                                                <img className={classes.Logo} src={Logo} alt=""/>
-                                            </Link>
-                                        </div>
+
+            <div className={'row'}>
+                <header className={classes.Header}>
+                    <div className="col-md-8 col-xs-2">
+                        <Navigation/>
+                        <NavigationMobile/>
+                    </div>
+                    <div className="col-md-4 col-xs-10">
+                        <div className={'row'}>
+                            <div className={classes.Left}>
+                                <div className="col-md-6 col-xs-6">
+                                    <div className={classes.BoxCart}>
+                                        <CartBoxHeader/>
                                     </div>
                                 </div>
-
+                                <div className="col-md-6 col-xs-6">
+                                    <div className={classes.BoxLogo}>
+                                        <Link to="/">
+                                            <img className={classes.Logo} src={Logo} alt=""/>
+                                        </Link>
+                                    </div>
+                                </div>
                             </div>
+
                         </div>
-                    </header>
-                </div>
+                    </div>
+                </header>
+            </div>
 
-
+            <div className={'container'}>
                 <main className={classes.Main}>
                     {props.children}
                 </main>
-                <footer className={classes.Footer}>
-                    <Footer/>
-                </footer>
             </div>
+            <footer className={classes.Footer}>
+                <Footer/>
+            </footer>
+
         </div>
     )
 }
