755c1e44d16bb95dc022de4e7b3f8ab8d6895f88
Mohammad Hossein Vafaee <vafaee.mh@gmail.com>
2021-09-04 14:23:44 +0430

    working responsive app

--- src/app/Components/ServicesUs/ServicesUs.js
+++ src/app/Components/ServicesUs/ServicesUs.js
@@ -1,14 +1,15 @@
-import classes from './ServicesUs.module.scss';
+import classes from "./ServicesUs.module.scss";
 
-
-
-export default function ServicesUs({data}) {
-    return (
-        <div className={classes.ServicesUs}>
-            <div className={classes.Icon}>{data.icon}</div>
-            <h2 className={classes.Title}>{data.title}</h2>
-            <p className={classes.Paragraph}>{data.description}</p>
+export default function ServicesUs({ ListServicesUs }) {
+  return (
+    <>
+      {ListServicesUs.map((item, index) => (
+        <div key={index} className={classes.ServicesUs}>
+          <div className={classes.Icon}>{item.icon}</div>
+          <h2 className={classes.Title}>{item.title}</h2>
+          <p className={classes.Paragraph}>{item.description}</p>
         </div>
-
-    )
+      ))}
+    </>
+  );
 }
