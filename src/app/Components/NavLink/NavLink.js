31a815a857ec480e396f1317e44efa7012551362
Mohammad Hossein Vafaee <vafaee.mh@gmail.com>
2021-10-16 20:17:56 +0330

    added sub menu

--- src/app/Components/NavLink/NavLink.js
+++ src/app/Components/NavLink/NavLink.js
@@ -4,8 +4,6 @@ import classes from "./NavLink.module.scss";
 import { Constants } from "../../values/Constants";
 
 const LinkData = [
-  { to: "/", value: Constants.Splash },
-  { to: "/product", value: Constants.Product },
   { to: "/product/laptop", value: Constants.Laptop },
   { to: "/product/camera", value: Constants.Camera },
   { to: "/product/watch", value: Constants.Watch },
@@ -13,17 +11,27 @@ const LinkData = [
 export default function NavLink() {
   return (
     <ul className={classes.Items}>
-      {LinkData.map((link, index) => (
-        <li
-          key={index}
-          className={classes.Item}
-      
-        >
-          <Link to={link.to} className={classes.Link}>
-            {link.value}
-          </Link>
-        </li>
-      ))}
+      <li className={classes.Item}>
+        <Link to="/" className={classes.Link}>
+          {Constants.Splash}
+        </Link>
+      </li>
+      <li className={classes.Item}>
+        <Link to="/product" className={classes.Link}>
+          {Constants.Product}
+        </Link>
+        <div className={classes.subNavigation}>
+          <ul className={classes.subItems}>
+            {LinkData.map((link, index) => (
+              <li key={index} className={classes.subItem}>
+                <Link to={link.to} className={classes.subLink}>
+                  {link.value}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </li>
     </ul>
   );
 }
